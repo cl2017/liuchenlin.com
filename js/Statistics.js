c39703ae6a8994e591549553e8ac232391f16c18
cl2017 <314498010@qq.com>
2016-11-10 23:56:02 +0800

    clear document
    整理所有文档 同步最近文件

--- js/Statistics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?238abaa09a35cf7d17ace4ed01a83943";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
